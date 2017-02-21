0302d626a81081a2158002a3fd2fb8e7b934b35c
MyNameIsntADAM <hazelton14113@yahoo.com>
2017-02-21 00:23:48 -0800

    Project2 Finished; Fixed random button

--- js/random.js
+++ js/random.js
@@ -3,10 +3,7 @@ window.onload = function() {
 	a.onclick = function() {
 		var num = document.querySelectorAll('.project').length;
 		var random = Math.floor(Math.random() * num) + 1;
-		/* Do this when you actually have three projects up
 		window.open('project' + random + '.html');
-		*/
-		window.open('project1.html');
 		return false;
 	}
 }
