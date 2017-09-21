120193a3a5e0031792418f5fb685f4c9dce15cdd
Adam Hazelton <hazeltonadam@gmail.com>
2017-09-21 00:28:48 -0400

    Added project3 (Bumble website) and put into folder bumble

--- js/random.js
+++ js/random.js
@@ -3,7 +3,8 @@ window.onload = function() {
 	a.onclick = function() {
 		var num = document.querySelectorAll('.project').length;
 		var random = Math.floor(Math.random() * num) + 1;
-		window.open('project' + random + '.html');
+		if(random != 3) window.open('project' + random + '.html');
+		else window.open('bumble/index.html');
 		return false;
 	}
 }
